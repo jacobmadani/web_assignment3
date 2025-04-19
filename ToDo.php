a4e89c62d0a856a6a63de14869d99bbcb63e0bb5
Yaacoub Elmadani <itsmadanii@gmail.com>
2025-04-19 22:00:37 +0300

    adding the testing samples

--- ToDo.php
+++ ToDo.php
@@ -19,7 +19,12 @@ function groupByStatus(array $tasks):array{
     }
     return $groupedTasks;
 }
-
+$tasks = [
+    ["id" => 1, "title" => "Buy groceries", "status" => "pending", "due" => "2024-04-15"],
+    ["id" => 2, "title" => "Submit report", "status" => "done", "due" => "2024-04-10"],
+    ["id" => 3, "title" => "Go jogging", "status" => "pending", "due" => "2024-04-12"],
+    ["id" => 4, "title" => "Book flight", "status" => "done", "due" => "2024-04-08"]
+];
 
 
 
